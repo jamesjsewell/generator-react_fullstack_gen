dc576439d1e2014158498a344f1827c5bdbb1b63
james sewell <jamesjosephsewell@gmail.com>
2018-12-11 12:06:32 -0600

    .

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,6 +16,7 @@ module.exports = class extends Generator {
     }).then(answers => {
       this.appName = answers.name
       this.destinationRoot(this.contextRoot + '/' + this.appName)
+      this.appRoot = this.destinationRoot()
       // this.appRoot = `${this.destinationRoot()}/${this.appName}`
       // this.destinationRoot(answers.name)
       done()
