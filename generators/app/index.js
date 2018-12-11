e9ba46d5d785c3b39537d57deca83db6561c7ef3
james sewell <jamesjosephsewell@gmail.com>
2018-12-11 12:29:26 -0600

    .

--- generators/app/index.js
+++ generators/app/index.js
@@ -51,13 +51,28 @@ module.exports = class extends Generator {
 
     copyer(['package.json', 'index.js', 'README.md'], null, null)
 
-    this.installDependencies({
-      npm: true,
-      bower: false,
-      yarn: false
+    // "axios": "0.18.0",
+    // "body-parser": "1.18.3",
+    // "express": "4.16.4",
+    // "helmet": "3.15.0",
+    // "mustache-express": "1.2.8"
+
+    // DEV
+    // "nodemon": "1.18.7",
+    // "concurrently": "4.1.0",
+    // "dotenv": "6.2.0",
+    // "create-react-app": "2.1.1"
+
+    this.npmInstall(['nodemon', 'concurrently', 'dotenv', 'create-react-app'], {
+      'save-dev': true
     })
 
-    this.spawnCommandSync('create-react-app', [
+    this.npmInstall(
+      ['axios', 'body-parser', 'express', 'helmet', 'mustache-express'],
+      { save: true }
+    )
+
+    this.spawnCommand('create-react-app', [
       'client',
       '--scripts-version',
       'digitalcrafts-react-scripts'
